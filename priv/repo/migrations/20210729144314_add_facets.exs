8faa634380282a5b1d7195c119f751ce2f2fcdce
jherve <julien.jev.herve@gmail.com>
2021-08-01 17:39:22 +0200

    Add facets/show_occurrence table

---
+++ priv/repo/migrations/20210729144314_add_facets.exs
@@ -0,0 +1,21 @@
+defmodule MediaWatch.Repo.Migrations.AddFacets do
+  use Ecto.Migration
+
+  def change do
+    create table(:facets) do
+      add :parsed_snapshot_id, references(:parsed_snapshots, column: :id), null: false
+      add :date_start, :utc_datetime, null: false
+      add :date_end, :utc_datetime, null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create table(:show_occurrences, primary_key: false) do
+      add :id, references(:facets, column: :id), primary_key: true
+
+      add :title, :string, null: false
+      add :description, :string, null: false
+      add :url, :string, null: false
+    end
+  end
+end
