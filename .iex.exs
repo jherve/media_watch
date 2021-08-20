5230fc5dd29a871cdfcdeb58c2d0e22f0f5bc874
jherve <julien.jev.herve@gmail.com>
2021-08-20 13:28:08 +0200

    Add Analysis module aliases to prompt

--- .iex.exs
+++ .iex.exs
@@ -1,10 +1,14 @@
 import Ecto.Changeset
-alias MediaWatch.Repo
-alias MediaWatch.{Catalog, Snapshots, Http}
-alias MediaWatch.Catalog.{Show, Item}
-alias MediaWatch.Catalog.Source
+
+alias MediaWatch.{Repo, Catalog, Snapshots, Parsing, Analysis, Http}
+
+alias MediaWatch.Catalog.{Show, Item, Source}
 alias MediaWatch.Catalog.Source.RssFeed
+
 alias MediaWatch.Snapshots.Snapshot
 alias MediaWatch.Snapshots.Snapshot.Xml
-alias MediaWatch.Parsing
+
 alias MediaWatch.Parsing.ParsedSnapshot
+
+alias MediaWatch.Analysis.Facet
+alias MediaWatch.Analysis.Facet.{ShowOccurrence, Description}
