2632c6be0968563456e847116624b3210165d82b
jherve <julien.jev.herve@gmail.com>
2021-09-16 21:05:46 +0200

    Handle description & show list display when nothing has been loaded yet

--- lib/web/live/item_live.ex
+++ lib/web/live/item_live.ex
@@ -20,22 +20,38 @@ defmodule MediaWatchWeb.ItemLive do
     do: ~L"""
       <h1><%= @item.show.name %></h1>
 
-      <dl>
-        <dt>URL</dt>
-        <dd><%= link @description.description.url, to: @description.description.url %></dd>
-        <dt>Description</dt>
-        <dd><%= @description.description.description %></dd>
-        <dt>Image</dt>
-        <dd><img src="<%= @description.description.image %>"/></dd>
-      </dl>
+      <%= render_description(assigns) %>
 
       <h2>Emissions</h2>
 
-      <ul>
-        <%= for o <- @occurrences do %>
-          <li><%= render_occurrence(o) %></li>
-        <% end %>
-      </ul>
+      <%= render_occurrences_list(assigns) %>
+    """
+
+  defp render_description(assigns = %{description: :error}),
+    do: ~L"<dl>Pas de description disponible</dl>"
+
+  defp render_description(assigns),
+    do: ~L"""
+    <dl>
+      <dt>URL</dt>
+      <dd><%= link @description.description.url, to: @description.description.url %></dd>
+      <dt>Description</dt>
+      <dd><%= @description.description.description %></dd>
+      <dt>Image</dt>
+      <dd><img src="<%= @description.description.image %>"/></dd>
+    </dl>
+    """
+
+  defp render_occurrences_list(assigns = %{occurrences: :error}),
+    do: ~L"<p>Pas d'émission disponible</p>"
+
+  defp render_occurrences_list(assigns),
+    do: ~L"""
+    <ul>
+      <%= for o <- @occurrences do %>
+        <li><%= render_occurrence(o) %></li>
+      <% end %>
+    </ul>
     """
 
   defp render_occurrence(o),
@@ -46,10 +62,13 @@ defmodule MediaWatchWeb.ItemLive do
     """
 
   defp group_facets(facets) do
-    %{{false, true} => [description], {true, false} => occurrences} =
-      facets
-      |> Enum.group_by(&{not is_nil(&1.show_occurrence), not is_nil(&1.description)})
+    case facets
+         |> Enum.group_by(&{not is_nil(&1.show_occurrence), not is_nil(&1.description)}) do
+      %{{false, true} => [description], {true, false} => occurrences} ->
+        {description, occurrences}
 
-    {description, occurrences}
+      %{} ->
+        {:error, :error}
+    end
   end
 end
