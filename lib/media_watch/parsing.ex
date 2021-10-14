ff65f29c8b00799192a6decdefdf406d8401943a
jherve <julien.jev.herve@gmail.com>
2021-10-14 12:57:41 +0200

    Remove now useless join on `get_parsed` function

--- lib/media_watch/parsing.ex
+++ lib/media_watch/parsing.ex
@@ -1,18 +1,12 @@
 defmodule MediaWatch.Parsing do
   import Ecto.Query
   alias MediaWatch.Repo
-  alias MediaWatch.Snapshots.Snapshot
   alias MediaWatch.Parsing.{ParsedSnapshot, Slice}
   @parsed_preloads [:xml, :source]
 
   def get_parsed(source_ids) when is_list(source_ids),
     do:
-      from(p in ParsedSnapshot,
-        join: s in Snapshot,
-        on: s.id == p.snapshot_id,
-        where: s.source_id in ^source_ids,
-        select: {s.source_id, p}
-      )
+      from(p in ParsedSnapshot, where: p.source_id in ^source_ids, select: {p.source_id, p})
       |> Repo.all()
 
   def get_slices(source_ids) when is_list(source_ids),
