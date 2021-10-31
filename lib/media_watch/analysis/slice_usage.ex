cdfea724ecb08be7de9aed6e22e3f579c13897f0
jherve <julien.jev.herve@gmail.com>
2021-10-31 19:55:00 +0100

    Update slice classification depending on html preview card type

--- lib/media_watch/analysis/slice_usage.ex
+++ lib/media_watch/analysis/slice_usage.ex
@@ -35,6 +35,13 @@ defmodule MediaWatch.Analysis.SliceUsage do
 
   def classify(%Slice{type: :rss_channel_description}), do: :item_description
   def classify(%Slice{type: :rss_entry}), do: :show_occurrence_description
-  def classify(%Slice{type: :html_preview_card}), do: :show_occurrence_description
+
+  def classify(%Slice{type: :html_preview_card, html_preview_card: %{type: card_type}})
+      when card_type in [:excerpt, :excerpt_short],
+      do: :show_occurrence_excerpt
+
+  def classify(%Slice{type: :html_preview_card, html_preview_card: %{type: _}}),
+    do: :show_occurrence_description
+
   def classify(%Slice{type: :open_graph}), do: :item_description
 end
