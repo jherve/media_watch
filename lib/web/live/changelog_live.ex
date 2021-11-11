62ecb1bbaf92edcaee65e96704e8f9973a22915c
jherve <julien.jev.herve@gmail.com>
2021-11-11 19:44:31 +0100

    fix: Load user-changelog file at runtime instead of as a module attribute

--- lib/web/live/changelog_live.ex
+++ lib/web/live/changelog_live.ex
@@ -3,7 +3,6 @@ defmodule MediaWatchWeb.ChangelogLive do
   # this when the page could be generated statically during compilation,
   # but that will do for now.
   use MediaWatchWeb, :live_view
-  @changelog_path Application.app_dir(:media_watch, "priv/CHANGELOG-USER.md")
 
   @impl true
   def mount(_params, _session, socket), do: {:ok, socket}
@@ -17,8 +16,13 @@ defmodule MediaWatchWeb.ChangelogLive do
     """
 
   def get_changelog() do
-    with {:ok, content} <- File.read(@changelog_path),
-         {:ok, as_html, _} <- Earmark.as_html(content),
-         do: as_html |> Phoenix.HTML.raw()
+    changelog_path = Application.app_dir(:media_watch, "priv/CHANGELOG-USER.md")
+
+    with {:ok, content} <- File.read(changelog_path),
+         {:ok, as_html, _} <- Earmark.as_html(content) do
+      as_html |> Phoenix.HTML.raw()
+    else
+      _ -> "Mmh, il y a eu un petit problème.."
+    end
   end
 end
